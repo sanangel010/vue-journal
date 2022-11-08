f4f3b7fb2379f3c1df3dac77334e7a79eefa99d5
Jose Angel Haro Juarez <sanangel010@gmail.com>
2022-11-07 23:05:19 -0600

    Se adicionan esqueleto del journal

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,9 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "strict": "off",
+    'no-unused-vars': 'off',
+    "vue/multi-word-component-names": "off"
   }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,9 +4,20 @@
     
     <h1>Hola Gallo !!!</h1>
 
-    <button class="btn btn-primary">Primary</button>
-    <button class="btn btn-secondary">Secondary</button>
-    <button class="btn btn-success">Success</button>
+    <button @click="goToDaybook" class="btn btn-primary">Primary</button>
+    <button @click="goToDaybook" class="btn btn-secondary">Secondary</button>
+    <button @click="goToDaybook" class="btn btn-success">Success</button>
 
   </div>
 </template>
+
+
+<script>
+  export default {
+    methods: {
+      goToDaybook() {
+        this.$router.push( { name: 'no-entry' } )
+      }
+    }
+  }
+</script>
